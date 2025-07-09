61c3cd9d50ba5f259c759ff4833c808568e9e870
aleksey.shumlyaev <aleksey.shumlyaev@rhinocodes.com>
2025-07-10 00:38:46 +0300

    feat: added functions

---
+++ rust-book/functions/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, functions!");
+
+    another_function(1, 'Y');
+
+    test_expression()
+}
+
+fn another_function(x: i32, y: char) {
+    println!("Another function.");
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn test_expression() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+}
